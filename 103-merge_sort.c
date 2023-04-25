ea7a3b5a9b3bea80b73886e17a6c8a76a5d7424b
Noahxyzs <noahsolomon528@yahoo.com>
2023-04-25 12:25:26 -0700

    Big O

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,83 +1,144 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "sort.h"
 
 /**
- * merge_compare - compares merges
- * @array: the integer array to sort
- * @start: the start index
- * @stop: the stop index
- * @new: the output array
+ * copy - copy
  *
- * Return: void.
+ * @src: src
+ * @dst: dst
+ * @size: size
+ *
+ * Return: Nothing
  */
-void merge_compare(int *array, size_t start, size_t stop, int *new)
+void copy(int *src, int *dst, int size)
 {
-	size_t i = start, j, k, mid;
+	int i;
 
-	j = mid = (start + stop) / 2;
+	for (i = 0; i < size; i++)
+		dst[i] = src[i];
+}
+/**
+ * merge - merges
+ *
+ * @array: array
+ * @buff: buff
+ * @minL: minl
+ * @maxL: maxl
+ * @minR: minr
+ * @maxR: maxr
+ *
+ * Return: Nothing
+ */
+void merge(int *array, int *buff, int minL, int maxL, int minR, int maxR)
+{
+	int i = minL, j = minR, k = minL;
+
+	while (i <= maxL || j <= maxR)
+
+		if (i <= maxL && j <= maxR)
+			if (buff[i] <= buff[j])
+				array[k] = buff[i], k++, i++;
+			else
+				array[k] = buff[j], k++, j++;
+
+		else if (i > maxL && j <= maxR)
+			array[k] = buff[j], k++, j++;
+		else
+			array[k] = buff[i], k++, i++;
+}
+/**
+ * printcheck - printcheck
+ *
+ * @array: array
+ * @r1: int
+ * @r2: int
+ *
+ * Return: Nothing
+ */
+void printcheck(int *array, int r1, int r2)
+{
+	int i;
+
+	for (i = r1; i <= r2; i++)
+	{
+		if (i > r1)
+			printf(", ");
+		printf("%d", array[i]);
+	}
+	printf("\n");
+}
+/**
+ * split - split
+ *
+ * @array: array
+ * @buff: buff
+ * @min: min
+ * @max: max
+ * @size: size
+ *
+ * Return: Nothing
+ */
+void split(int *array, int *buff, int min, int max, int size)
+{
+	int mid, tmax, minL, maxL, minR, maxR;
+
+	if ((max - min) <= 0)
+		return;
+
+	mid = (max + min + 1) / 2;
+	tmax = max;
+	max = mid - 1;
+
+	minL = min;
+	maxL = max;
+
+	split(array, buff, min, max, size);
+
+	min = mid;
+	max = tmax;
+
+	minR = min;
+	maxR = max;
+
+	split(array, buff, min, max, size);
+
 	printf("Merging...\n");
 	printf("[left]: ");
-	print_array(array + start, mid - start);
+
+	printcheck(array, minL, maxL);
+
 	printf("[right]: ");
-	print_array(array + mid, stop - mid);
-	for (k = start; k < stop; k++)
-		if (i < mid && (j >= stop || array[i] <= array[j]))
-		{
 
-			new[k] = array[i++];
-		}
-		else
-		{
-			new[k] = array[j++];
-		}
+	printcheck(array, minR, maxR);
+	merge(array, buff, minL, maxL, minR, maxR);
+	copy(array, buff, size);
+
 	printf("[Done]: ");
-	print_array(new + start, stop - start);
+	printcheck(array, minL, maxR);
 }
-
 /**
- * divide - divide
- * @array: array
- * @start: start
- * @stop: stop
- * @new: new
+ * merge_sort - merge_sort
  *
- * Return: void
- */
-void divide(int *array, size_t start, size_t stop, int *new)
-{
-	size_t mid;
-
-	mid = (start + stop) / 2;
-	if (stop - start < 2)
-	{
-		return;
-	}
-	divide(new, start, mid, array);
-	divide(new, mid, stop, array);
-	divide(new, start, stop, array);
-}
-
-
-/**
- * merge_sort - merge sort
  * @array: array
  * @size: size
  *
- * Return: void
+ * Return: Nothing
  */
 void merge_sort(int *array, size_t size)
 {
-	int *new;
-	size_t i;
+	int *buff;
 
-
-	if (!array || size < 2)
+	if (size < 2)
 		return;
 
-	new = malloc(sizeof(int) * size);
-	if (!new)
+	buff = malloc(sizeof(int) * size);
+	if (buff == NULL)
 		return;
-	for (i = 0; i < size; i++)
-		new[i] = array[i];
-	divide(array, 0, size, new);
-	free(new);
+
+	copy(array, buff, size);
+
+	split(array, buff, 0, size - 1, size);
+
+	free(buff);
 }
